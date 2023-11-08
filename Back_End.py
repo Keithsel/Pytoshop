48118e13e640051777db090ae6d923ae5be6c154
Chunporo <hackingoxo@gmail.com>
2023-11-08 08:47:06 +0700

    add

--- Back_End.py
+++ Back_End.py
@@ -3,7 +3,7 @@ import cv2
 import os
 import random
 import os
-import nicegui as n
+import uuid
 
 
 def read_img(path):
@@ -15,8 +15,11 @@ def read_img(path):
 
 def output_img(image, num, output="output"):
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    output = os.path.join(os.path.join(os.path.dirname(__file__), output))
-    cv2.imwrite("img" + num + ".jpg",image)
+    output_dir = os.path.join(os.path.dirname(__file__), output)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    output_path = os.path.join(output_dir, f"{str(uuid.uuid4())}" + ".jpg")
+    cv2.imwrite(output_path, image)
 
 
 def apply_geometric_transform(image, rotate, shift, scale, elastic):
@@ -34,32 +37,65 @@ def apply_geometric_transform(image, rotate, shift, scale, elastic):
     return transformed_image
 
 
-def color_space_transformations(image,Hue,RGBs,Brightness,shuffle,Cla):
-    transform = A.Compose([
-        A.HueSaturationValue(p=Hue),
-        A.RGBShift(r_shift_limit=25, g_shift_limit=25, b_shift_limit=25, p=RGBs),
-        A.RandomBrightnessContrast(brightness_limit=0.2, contrast_limit=0.2, p=Brightness),
-        A.ChannelShuffle(p=shuffle),
-        A.CLAHE(p=Cla),
-    ])
+def color_space_transformations(image, Hue, RGBs, Brightness, shuffle, Cla):
+    transform = A.Compose(
+        [
+            A.HueSaturationValue(p=Hue),
+            A.RGBShift(r_shift_limit=25, g_shift_limit=25, b_shift_limit=25, p=RGBs),
+            A.RandomBrightnessContrast(
+                brightness_limit=0.2, contrast_limit=0.2, p=Brightness
+            ),
+            A.ChannelShuffle(p=shuffle),
+            A.CLAHE(p=Cla),
+        ]
+    )
 
     transformed = transform(image=image)["image"]
 
     return transformed
-def kernel_filters(image,blur,meblur,gaublur,motion,emboss):
-    transform = A.Compose([
-        A.Blur(blur_limit=(1, 3),p=blur),
-        A.MedianBlur(blur_limit=3,p=meblur),
-        A.GaussianBlur(blur_limit=3,p=gaublur),
-        A.MotionBlur(blur_limit=3,p=motion),
-        A.Emboss(alpha=(0.5, 1.0), strength=(1.0, 2.0),p=emboss),
-    ])
+
+
+def kernel_filters(image, blur, meblur, gaublur, motion, emboss):
+    transform = A.Compose(
+        [
+            A.Blur(blur_limit=(1, 3), p=blur),
+            A.MedianBlur(blur_limit=3, p=meblur),
+            A.GaussianBlur(blur_limit=3, p=gaublur),
+            A.MotionBlur(blur_limit=3, p=motion),
+            A.Emboss(alpha=(0.5, 1.0), strength=(1.0, 2.0), p=emboss),
+        ]
+    )
 
     transformed = transform(image=image)["image"]
 
     return transformed
-def back_end(path,transformation_functions=[apply_geometric_transform, color_space_transformations, kernel_filters],rotate=0.5,shift=0.5,scale=0.5,elastic=0.5,blur=0.5,meblur=0.5,gaublur=0.5,motion=0.5,emboss=0.5,Hue=0.5,RGBs=0.5,Brightness=0.5,shuffle=0.5,Cla=0.5):
-    for i in range(100):
+
+
+def back_end(
+    path,
+    number_of_images=10,
+    transformation_functions=[
+        apply_geometric_transform,
+        color_space_transformations,
+        kernel_filters,
+    ],
+    
+    rotate=0.5,
+    shift=0.5,
+    scale=0.5,
+    elastic=0.5,
+    blur=0.5,
+    meblur=0.5,
+    gaublur=0.5,
+    motion=0.5,
+    emboss=0.5,
+    Hue=0.5,
+    RGBs=0.5,
+    Brightness=0.5,
+    shuffle=0.5,
+    Cla=0.5,
+):
+    for i in range(number_of_images):
         image = read_img(path)
         selected_functions = random.sample(
             transformation_functions,
@@ -73,6 +109,8 @@ def back_end(path,transformation_functions=[apply_geometric_transform, color_spa
                     image, Hue, RGBs, Brightness, shuffle, Cla
                 )
             else:
-                image = kernel_filters(image,blur,meblur,gaublur,motion,emboss)
-        output_img(image,str(i))
-back_end("314218660_458182969777595_2920542176920776199_n.jpg")
+                image = kernel_filters(image, blur, meblur, gaublur, motion, emboss)
+        output_img(image, str(i))
+
+
+# back_end("img/1699381033.jpg")
--- main.py
+++ main.py
@@ -4,15 +4,22 @@ from Back_End import *
 import matplotlib.pyplot as plt
 from PIL import Image
 import io
-import time
+import uuid
 import shutil
+import glob
+import os
 
 # import pygetwindow as gw
 
+img_folder = "img"
+if not os.path.exists(img_folder):
+    os.makedirs(img_folder)
 
-def clear_folder(folder_path):
-    if os.path.exists(folder_path):
-        shutil.rmtree(folder_path)
+
+def remove_all_files(folder_path):
+    files = glob.glob(os.path.join(folder_path, "*"))
+    for f in files:
+        os.remove(f)
 
 
 def show(event: ValueChangeEventArguments):
@@ -25,6 +32,8 @@ with ui.row():
     ui.label("Switch mode:")
     ui.button("Dark", on_click=dark.enable)
     ui.button("Light", on_click=dark.disable)
+    ui.label("Image Augmentation").classes("text-2xl font-bold text-left")
+
 
 with ui.card().classes("w-full items-center"):
     with ui.dialog().props("full-width") as dialog:
@@ -35,37 +44,130 @@ with ui.card().classes("w-full items-center"):
         img = Image.open(io.BytesIO(e.content.read()))
         # plt.imshow(img)
         # plt.show()
-        img_folder = "img"
-        if not os.path.exists(img_folder):
-            os.makedirs(img_folder)
-        timestamp = int(time.time())
-        img_path = os.path.join(img_folder, f"{timestamp}.jpg")
+        img_path = os.path.join(img_folder, f"{str(uuid.uuid4())}.jpg")
         img.save(img_path)
 
-    clear_folder("img")
-    ui.upload(multiple=True, on_upload=handle_upload).props("accept=image/*").classes(
-        "max-w-full"
-    )
+    ui.upload(multiple=True, on_upload=handle_upload).props("accept=image/*").classes("max-w-full")
 
     with ui.column():
-        ui.button("Apply Transformations")
-        ui.checkbox("apply_geometric_transform", on_change=show)
-        slider = ui.slider(min=0, max=100, value=50)
-    ui.label().bind_text_from(slider, "value")
-    ui.checkbox("color_space_transformations", on_change=show)
-    ui.checkbox("kernel_filters", on_change=show)
+        # ui.checkbox("apply_geometric_transform", on_change=show)
+        # slider = ui.slider(min=0, max=100, value=50)
+        # ui.label().bind_text_from(slider, "value")
+        # ui.checkbox("color_space_transformations", on_change=show)
+        # ui.checkbox("kernel_filters", on_change=show)
+        ui.label("Rotate")
+        rotateA = ui.slider(min=0, max=1, value=0.5, step=0.01)
+        ui.label().bind_text_from(rotateA, "value")
+
+        ui.label("Shift")
+        shiftA = ui.slider(min=0, max=1, value=0.5, step=0.01)
+        ui.label().bind_text_from(shiftA, "value")
+
+        ui.label("Scale")
+        scaleA = ui.slider(min=0, max=1, value=0.5, step=0.01)
+        ui.label().bind_text_from(scaleA, "value")
+
+        ui.label("Elastic")
+        elasticA = ui.slider(min=0, max=1, value=0.5, step=0.01)
+        ui.label().bind_text_from(elasticA, "value")
+
+        ui.label("Blur")
+        blurA = ui.slider(min=0, max=1, value=0.5, step=0.01)
+        ui.label().bind_text_from(blurA, "value")
+
+        ui.label("MEBlur")
+        meblurA = ui.slider(min=0, max=1, value=0.5, step=0.01)
+        ui.label().bind_text_from(meblurA, "value")
+
+        ui.label("GauBlur")
+        gaublurA = ui.slider(min=0, max=1, value=0.5, step=0.01)
+        ui.label().bind_text_from(gaublurA, "value")
+
+        ui.label("Motion")
+        motionA = ui.slider(min=0, max=1, value=0.5, step=0.01)
+        ui.label().bind_text_from(motionA, "value")
+
+        ui.label("Emboss")
+        embossA = ui.slider(min=0, max=1, value=0.5, step=0.01)
+        ui.label().bind_text_from(embossA, "value")
+
+        ui.label("Hue")
+        HueA = ui.slider(min=0, max=1, value=0.5, step=0.01)
+        ui.label().bind_text_from(HueA, "value")
+
+        ui.label("RGBs")
+        RGBsA = ui.slider(min=0, max=1, value=0.5, step=0.01)
+        ui.label().bind_text_from(RGBsA, "value")
+        # ui.button('Choose',on_click=lambda: show_numimg())
+        # label = ui.label()
+        NumberOfImages = ui.number(label='Number of Images',value=10,min=1,max=100,step=1)
+        def showVal(NumberOfImages):
+            internal_var = NumberOfImages.value
+            print(internal_var)
+            return int(internal_var)
+
+        ui.button(
+            "Apply Transformations",
+            on_click=lambda: apply_imgAug_to_all_images_in_folder(
+                os.path.join(os.path.dirname(__file__), "img")
+            ),
+        )
 
     def apply_imgAug_to_all_images_in_folder(folder_path):
+        # timestamp = ui.spinner("facebook", size="lg", color="green").classes(
+        #     "items-center w-full"
+        # )
+        remove_all_files("output")
         if os.path.exists(folder_path):
             for filename in os.listdir(folder_path):
-                if filename.endswith(".jpg") or filename.endswith(".png"): 
+                if filename.endswith(".jpg") or filename.endswith(".png"):
                     image_path = os.path.join(folder_path, filename)
-                    img = Image.open(image_path)
-                    imgAug(img)
+                    back_end(
+                        image_path, number_of_images=showVal(NumberOfImages),
+                        rotate=showVal(rotateA), 
+                        shift=showVal(shiftA), 
+                        scale=showVal(scaleA), 
+                        elastic=showVal(elasticA), 
+                        blur=showVal(blurA), 
+                        meblur=showVal(meblurA), 
+                        gaublur=showVal(gaublurA), 
+                        motion=showVal(motionA), 
+                        emboss=showVal(embossA), 
+                        Hue=showVal(HueA), 
+                        RGBs=showVal(RGBsA)
+                    )  # Pass image_path instead of img
+                    ui.notify(f"Success with {filename}", type="positive")
+            shutil.make_archive(
+                "output_img",
+                "zip",
+                os.path.join(os.path.dirname(__file__), "output"),
+            )
+            remove_all_files("img")
+            # timestamp.delete( )
         else:
             print(f"The folder {folder_path} does not exist.")
-        
-    clear_folder("output")
-    apply_imgAug_to_all_images_in_folder("img")
 
+    ui.button(
+        "Download Result",
+        on_click=lambda: ui.download(
+            os.path.join(os.path.dirname(__file__), "output_img.zip"),
+        ),
+    )
+
+
+# apply_imgAug_to_all_images_in_folder('img')
+def show_images():
+    output_folder = os.path.join(os.path.dirname(__file__), "output")
+    images = [
+        os.path.join(output_folder, f)
+        for f in os.listdir(output_folder)
+        if f.endswith(".jpg")
+    ]
+    with ui.grid(columns=10):
+        for image in images:
+            ui.image(image).classes("w-20")
+    # return gallery
+
+
+ui.button("Show Images", on_click=show_images, color="green")
 ui.run()
